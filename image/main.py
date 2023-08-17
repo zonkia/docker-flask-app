7bfe765d90a483fb88ef5c2728e848c65df4ebf0
Marcin Stanczyk <mstanc@softserveinc.com>
2023-08-17 15:48:24 +0200

    fail test

--- image/main.py
+++ image/main.py
@@ -42,4 +42,4 @@ def km_from_miles(miles):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
