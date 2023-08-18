15dc36101930391a4902994bad0a939b2f456ab6
Marcin Stanczyk <mstanc@softserveinc.com>
2023-08-18 13:23:38 +0200

    test#patch

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,11 @@ output "instance_address" {
   value       = "http://${aws_instance.terra-app-instance.public_ip}:${var.app_port}"
   description = "App address"
 }
-output "eip_address" {
+output "app_address" {
   value       = "http://${aws_eip.eip.public_ip}:${var.app_port}"
   description = "App address"
+}
+output "eip_address" {
+  value       = aws_eip.eip.public_ip
+  description = "EIP address"
 }
