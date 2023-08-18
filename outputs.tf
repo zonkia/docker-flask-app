36a71e93fa8642868598272fddf8cd3fbdbc536c
Marcin Stanczyk <mstanc@softserveinc.com>
2023-08-18 09:48:24 +0200

    test#patch

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@ output "app_address" {
   value       = "http://${aws_instance.terra-app-instance.public_ip}:${var.app_port}"
   description = "App address"
 }
+output "ecr_registry" {
+  value       = var.ecr_registry
+  description = "App address"
+}
+output "image_name" {
+  value       = var.image_name
+  description = "App address"
+}
